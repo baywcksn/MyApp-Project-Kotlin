229c38111f0a98968e581c7e88238c6cbcb6e47c
Bayuwcksn14 <bayuwcksn01@gmail.com>
2025-01-26 22:23:46 +0700

    Refactor: Implement edge-to-edge display in MainActivity
    - Remove `enableEdgeToEdge`, `ViewCompat`, and `WindowInsetsCompat` imports as they are no longer needed for handling edge-to-edge behavior.
    - Update the code to rely on the default edge-to-edge behavior introduced in Android 15, eliminating the need for manual edge-to-edge configuration.

--- app/src/main/java/com/example/myapp_project_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/myapp_project_kotlin/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.myapp_project_kotlin
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
